1b59dee9dfd37bbc3e32a8c4e1e26c70557075b5
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-01-14 12:07:40 +0000

    loading

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,11 +5,12 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -21,31 +19,82 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
+def get_user():
+    users = User.query.all()
+    results = [user.serialize() for user in users]
+    response_body = {"message": "ok",
+                     "total_records": len(results),
+                     "results": results}
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    user = User(email = request_body['email'],
+                password = request_body['password'],
+                is_active = request_body['is_active'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(request_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    results = [character.serialize() for character in people]
+    response_body = {"message": "ok",
+                     "total_records": len(results),
+                     "results": results}
+    return jsonify(response_body), 200
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_by_id(people_id):
+    people = db.get_or_404(People, people_id)
+    result = people.serialize()
+    response_body = {"message": "ok",
+                     "result": result}
+    return jsonify(response_body), 200
+
+
+@app.route('/planet', methods=['GET'])
+def get_planet():
+    planets = Planet.query.all()
+    results = [planet.serialize() for planet in planets]
+    response_body = {"message": "ok",
+                     "total_records": len(results),
+                     "results": results}
+    return jsonify(response_body), 200
+
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    planet = db.get_or_404(Planet, planet_id)
+    result = planet.serialize()
+    response_body = {"message": "ok",
+                     "result": result}
+    return jsonify(response_body), 200
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,8 +14,51 @@ class User(db.Model):
         return '<User %r>' % self.username
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        return {"id": self.id,
+                "email": self.email,
+                "is_active": self.is_active}
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    hair_color = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "hair_color": self.hair_color}
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name}
+
+
+class UserFavoritesPlanets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeingKey('user.id'), unique=True, nullable=False)
+    planets_id = db.Column(db.Integer, db.ForeingKey('planet.id'), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<UserFavoritesPkanets %r>' % self.id
+    
+    def serialize(self):
+        return {"id": self.id,
+                "user_id": self.user_id,
+                "planets_id": self.planets_id}
+
+    # body = request.get_json()
+    # favorite = UserFavoritesPlanets(person_id= body["person_id"], planets_id = body['id_planet'])
+    # db.session.add(favorite)
+    # db.session.commit()
